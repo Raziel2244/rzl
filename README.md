c8a8f608a0d68ef679875ade1ec34ac53dc85284
Elliot <public@raziel.space>
2019-08-22 09:09:59 +0100

    ready for release 1.0.0

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-# [RZL](https://raziel.dev) &middot; ![tag](https://img.shields.io/badge/-pre--alpha-red) ![GitHub code size in bytes](https://img.shields.io/github/languages/code-size/Raziel2244/rzl) [![GitHub license](https://img.shields.io/github/license/Raziel2244/rzl)](https://github.com/Raziel2244/rzl)
+# [RZL](https://raziel.dev) &middot; ![GitHub release (latest SemVer including pre-releases)](https://img.shields.io/github/v/release/Raziel2244/rzl?logo=github&logoColor=white&sort=semver) ![GitHub code size in bytes](https://img.shields.io/github/languages/code-size/Raziel2244/rzl) [![GitHub license](https://img.shields.io/github/license/Raziel2244/rzl)](https://github.com/Raziel2244/rzl)
 
 RZL is a lightweight modular JS Framework
 
-THIS IS AN **ALPHA VERSION** AND SO IS CURRENTLY IN DEVELOPMENT  
-ANYTHING BUILT USING AN ALPHA VERSION WILL NEED HEAVY REWORKING
+**Release v1.0.0 available now**
+
+__Documetation is being written currently__
--- examples/uiDef.js
+++ examples/uiDef.js
@@ -1,31 +1,39 @@
 // UI definition example
-var example = {
-  uis: {
-    testui: {
-      meta: {},
-      view: {
-        0: {
-          type: '',
-          style: {},
-          children: {
-            0: {
-              tag: '',
-              id: '',
-              class: '',
-              style: {},
-              title: '',
-              content: '',
-              placeholder: '',
-              inputtype: '',
-              imgsrc: '',
-              events: {},
-              props: {},
-              options: {},
-              selected: false
-            }
+const myApp = {
+  myAppModule : {
+    render : function() {
+      const myUI = new rzl.UI(this.uiDef); // create the new UI and store it
+      const myUI === myApp.state.myAppModule; // state is also stored globally
+    },
+
+    uiDef : {
+      meta : {
+        name : "myAppModule",
+        domain : "myApp",
+        pnode : document.body
+      },
+      view : {
+        style : { margin : "20px", "margin-top" : "5px" },
+        children : [
+          {
+            tag : "h1", // element tag - default is div
+            id : "title",
+            class : "red bold",
+            style : { margin : "20px", "margin-top" : "5px" },
+            title : "",
+            content : "",
+            placeholder : "",
+            inputtype : "",
+            imgsrc : "",
+            events : {
+              "click" : "myApp.myAppModule.function"
+            },
+            props : {}, // html element properties and custom properties
+            options : {},
+            selected : false
           }
-        }
+        ]
       }
-    }
+    },
   }
 }
